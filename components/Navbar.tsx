560605d9473dee37acddc97896d817f9af131de5
vijaysolanki <vijay_solanki@gecg28.ac.in>
2024-04-10 23:12:25 +0530

    fixed fabric issue

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,13 +3,32 @@
 import Image from "next/image";
 import { memo } from "react";
 import ActiveUsers from "./Users/ActiveUsers";
-import { NavbarProps } from "@/types/type";
+import { ActiveElement, NavbarProps } from "@/types/type";
+import { navElements } from "@/constants";
 // { activeElement, imageInputRef, handleImageUpload, handleActiveElement }: NavbarProps
 
 const Navbar = () => {
   return (
     <nav className="flex select-none items-center justify-between gap-4 bg-primary-black px-5 text-white py-3">
         <Image src="/assets/logo.svg" alt="FigPro Logo" width={58} height={20} />
+        <ul className="flex flex-row">
+          {
+            navElements.map((item : ActiveElement | any) => (
+              <li>
+                {
+                  Array.isArray(item.name) ? (
+                    <ShapeMenu />
+                  ) : item?.value === 'comments' ? (
+                    <NewThread></NewThread>
+                  ) : (
+                    <button></button>
+                  )
+                }
+              </li>
+            ))
+          }
+          
+        </ul>
         <ActiveUsers />
     </nav>
   )
